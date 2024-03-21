b4298e69dca5a685ee598bb82def76012fe7e633
Tsu <126349790+1Tsubasa@users.noreply.github.com>
2024-03-21 18:45:10 +0100

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    token: "MTE5MTgwNjkyNjUxNDgxNDk3Ng.GH_jfP.PsdP7C0WzleRo3SNz0_76PfmI04TPfyp8u_e-I",
+    bot_id: "1191806926514814976",
+    buyer: '1072553881134972970',
+    prefix: "+",
+    isPublic: false,
+    testVersion: true,
+    localhost: true,
+    panel: "localhost:30137",
+    support: 'https://discord.gg/clarityfr',
+    default_color: "#3535f8",
+    footer: {
+        text: "ζ͜͡Clarity"
+    },
+   devs: ["1072553881134972970", '378594821721554947', "171356978310938624"],
+    ownerClari: ["1072553881134972970", '378594821721554947', "564715031321772032" , "784432231371374624", "226017895963033601"],
+    respClari: ["171356978310938624",  "1148560858054656020", "1070807183152910366"],
+    marketClari: [],
+    designClari: ["993629285325733971"],
+    techClari: ["146056203397627904", "214339832061624320", "233657223190937601", "1127594734219710504", "842369097572024330", "366673880628199426", "1099042785736282205"],
+    bump: {
+        serv: "1160135417383891025",
+        cl: "1160357865610018848"
+    },
+    lavalink: {
+        host: "game02.codingheberg.com",
+        port: 5006,
+        password: "codinghebergbydok",
+        secure: false,
+        retryAmount: 200,
+        retryDelay: 40,
+        id: "Clarity"
+    },
+    express: {
+        server: true,
+        port: 3000
+    },
+    music: {
+        default_volume: 20,
+        loop: false,
+        autoplay: false,
+        leaveOnEnd: false,
+        leaveOnStop: false,
+        leaveOnEmpty: false,
+        "24/7": false,
+        serverdeaf: true
+    }
+}
